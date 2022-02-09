536644dd92a8c538ac369d05259ad1846344d1eb
Mari <marignacia.flores@gmail.com>
2022-02-09 23:56:03 +0100

    Add scrollUp, change routes

--- src/main.js
+++ src/main.js
@@ -5,3 +5,10 @@
 document.getElementById("nav-toggle").onclick = function() {
   document.getElementById("nav-content").classList.toggle("hidden");
 };
+
+/*ScrollUp*/
+
+const scrollup = document.querySelector("#toTopButton");
+scrollup.onclick = function() {
+  window.scrollTo({top:0, behavior: 'smooth'});
+};
